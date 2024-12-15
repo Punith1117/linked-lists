e276cb34cd002f4a47bfde8cf5dd3602899e4311
Punith1117 <punith11172006@gmail.com>
2024-12-15 14:35:58 +0530

    add insertAtEnd method to LinkedList class

--- index.js
+++ index.js
@@ -16,4 +16,14 @@ class LinkedList {
         this.head = new Node(data, this.head);
         this.size++;
     }
+
+    //insert node at the end
+    insertAtEnd(data) {
+        let node = this.head;
+        while (node.next) {//this runs until node.next is equal to null
+            node = node.next
+        }
+        node.next = new Node(data);
+        this.size++;
+    }
 }
